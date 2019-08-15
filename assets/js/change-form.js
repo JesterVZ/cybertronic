ec57a8039fa23c203193d784b79c76c1c2ab0be3
VLadimir Zhulin <vladimir.a.zhulin@gmail.com>
2019-08-15 13:53:21 +0300

    fixed fonts

---
+++ assets/js/change-form.js
@@ -0,0 +1,8 @@
+function showReg(){
+    document.getElementById('login-form').style.display ='block';
+    document.getElementById('reg-form').style.display ='none';
+}
+function showLogin(){
+    document.getElementById('login-form').style.display ='none';
+    document.getElementById('reg-form').style.display ='block';
+}
