e44591a407c8f2448c12711820b71433de49e79b
VLadimir Zhulin <vladimir.a.zhulin@gmail.com>
2019-08-15 14:40:23 +0300

    delete files

--- js/change-form.js
+++
@@ -1,8 +0,0 @@
-function showReg(){
-    document.getElementById('login-form').style.display ='block';
-    document.getElementById('reg-form').style.display ='none';
-}
-function showLogin(){
-    document.getElementById('login-form').style.display ='none';
-    document.getElementById('reg-form').style.display ='block';
-}
